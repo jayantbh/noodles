8de8dc6a6dbd0550ba17f8e202d4f5ad7bc9ae91
Jayant Bhawal <bhawal.jayant@gmail.com>
2016-03-16 17:27:10 +0530

    Basic working version.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var bodyParser = require("body-parser");
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
@@ -14,13 +14,34 @@ app.get("/", function (req, res) {
 });
 
 app.post("/scrape", function (req, res) {
-	var data = JSON.parse(req.body);
+	var data = req.body;
 	var url = data.url;
 	var type = data.type;
-	var selectors = data.selectors;
+	var selectors = data.selectors.split("\n");
+	var params = data.params;
+	var heads = JSON.parse(data.heads);
+	heads["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36";
+	if (type == "post") {
+		request.post({
+			url: url,
+			headers:heads,
+			form: params
+		}, function (err, httpResponse, body) { /* ... */
+			if(err){
+				res.send(err);
+			}
+			else{
+				var $ = cheerio.load(body);
 
-	if(type == "post"){
+				var responseHTML = "";
 
+				selectors.forEach(function (select, i) {
+					responseHTML += $(select).append($('#xxx').clone()).html();
+				});
+
+				res.send(responseHTML);
+			}
+		})
 	}
 });
 
